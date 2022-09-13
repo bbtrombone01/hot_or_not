bb61da3d6367b038321085671dcc6474f0d17631
bbtrombone <bbtrombone@sbcglobal.net>
2022-09-13 13:27:36 -0700

    removed cross orgin form imag tag in Image.js

--- src/Image.js
+++ src/Image.js
@@ -5,7 +5,6 @@ const Image =(props) =>{
     return <div className="thermoImageParent">
         <img 
             className="thermoImage"
-            crossOrigin= "anonymous"
             src={`https://whispering-ridge-23084.herokuapp.com/${props.image}`} alt="testing" />
     </div>
 }
